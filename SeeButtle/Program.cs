1e260fe8b5ecf2d163ed024c21a87bf374486e1c
baloxegal <baloxegal@gmail.com>
2021-03-06 14:44:02 +0200

    Adding new variant of Render and Combat Formation functions

--- SeeButtle/Program.cs
+++ SeeButtle/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SeeButtle
 {
@@ -10,9 +12,8 @@ namespace SeeButtle
             Map gameMap = new Map();
             gameMap.Render();
 
-            gameMap.myNavy.CombatFormation();
+            gameMap.myNavy.CombatFormation(gameMap);
 
-            gameMap.Render();
         }
     }    
 }
@@ -55,37 +56,27 @@ namespace Things
 
     class Map
     {
-        static public int width = 10;
-        static public int height = 10;
-        static public string xCoord = "abcdefghijklmnopqrstuvwxyz";
+        public const int SIZE = 10;        
+        public const string X_Coord = "abcdefghijklmnopqrstuvwxyz";        
         public Navy myNavy = new Navy();
 
         public void Render()
         {
             var b = "";
-            for (int i = 0; i < width; i++)
+            for (int i = 0; i < SIZE; i++)
             {
-                b += xCoord[i] + " ";
+                b += X_Coord[i] + " ";
             }
-            Console.WriteLine(String.Format("{0, 4} {1, 20}", " ", b));
-            for (int y = 0; y < height; y++)
+            Console.WriteLine(String.Format("{0, 4}{1, 20}", " ", b));
+            for (int y = 0; y < SIZE; y++)
             {
-                Console.Write(String.Format("{0, 3} {1, 1} ", y + 1, " "));
-                for (int x = 0; x < width; x++)
-                {                    
-                    for (int i = 0; i < myNavy.oneDeck.Length; i++)
-                    {
-                        if (myNavy.oneDeck[i].X == xCoord[x] && myNavy.oneDeck[i].Y == y)
-                        {
-                            Console.Write("# ");
-                        }
-                        else
-                        {
-                            if(i == myNavy.oneDeck.Length - 1)
-                                Console.Write(".");                            
-                        }
-                    }
-                    Console.Write(" ");
+                Console.Write(String.Format("{0, 3}{1, 1}", y + 1, " "));
+                for (int x = 0; x < SIZE; x++)
+                {
+                    if(myNavy.CoordinatesList.Contains(KeyValuePair.Create(X_Coord[x], y)))
+                        Console.Write("# ");                        
+                    else
+                        Console.Write(". ");                   
                 }
                 Console.WriteLine();
             }
@@ -95,81 +86,87 @@ namespace Things
     {
         protected enum DeckQuant
         {
-            ONE_DECK = 1, TWO_DECK, THREE_DECK, FOUR_DECK
+            ONE_DECK = 1, TWO_DECK//, THREE_DECK, FOUR_DECK
         }
         protected enum ShipQuant
         {
-            FOUR_DECK = 1, THREE_DECK, TWO_DECK, ONE_DECK
+            /*FOUR_DECK = 1, THREE_DECK,*/ TWO_DECK = 1, ONE_DECK
         }
+        Dictionary<ShipQuant, int> ShipDictionary = new Dictionary<ShipQuant, int>();
+        public List<KeyValuePair<char, int>> CoordinatesList = new List<KeyValuePair<char, int>>();
         protected enum Direction
         {
             NORTH = 'N', SOUTH = 'S', WEST = 'W', EAST = 'E'
         }
-        public Ship[] oneDeck = new Ship[(int)ShipQuant.ONE_DECK];
-        public Ship[] twoDeck = new Ship[(int)ShipQuant.TWO_DECK];
-        public Ship[] threeDeck = new Ship[(int)ShipQuant.THREE_DECK];
-        public Ship[] fourDeck = new Ship[(int)ShipQuant.FOUR_DECK];
-
+        public Ship[][] myNavy;        
         public Navy()
         {
-            for (int i = 0; i < oneDeck.Length; i++)
+            ShipQuant[] ArrayShipQuant = (ShipQuant[])Enum.GetValues(typeof(ShipQuant));
+            DeckQuant[] ArrayDeckQuant = (DeckQuant[])Enum.GetValues(typeof(DeckQuant));            
+            for(int i = 0; i < ArrayDeckQuant.Length; i++)
             {
-                oneDeck[i] = new Ship { Len = (int)DeckQuant.ONE_DECK };
+                ShipDictionary.Add(ArrayShipQuant[ArrayShipQuant.Length - 1 - i], (int)ArrayDeckQuant[i]);
             }
-            for (int i = 0; i < twoDeck.Length; i++)
+            
+            myNavy = new Ship[ArrayShipQuant.Length][];
+            for (int i = 0; i < myNavy.Length; i++)
             {
-                twoDeck[i] = new Ship { Len = (int)DeckQuant.TWO_DECK };
-            }
-            for (int i = 0; i < threeDeck.Length; i++)
-            {
-                threeDeck[i] = new Ship { Len = (int)DeckQuant.THREE_DECK };
-            }
-            for (int i = 0; i < fourDeck.Length; i++)
-            {
-                fourDeck[i] = new Ship { Len = (int)DeckQuant.FOUR_DECK };
-            }
+                myNavy[i] = new Ship[(int)ArrayShipQuant[i]];
+                for(int j = 0; j <  myNavy[i].Length; j++)
+                {   
+                    ShipDictionary.TryGetValue(ArrayShipQuant[i], out int l);
+                    myNavy[i][j] = new Ship { Len = l };
+                }
+            }           
         }
-        public void CombatFormation()
-        {
-            for (int i = 0; i < oneDeck.Length; i++)
+        public void CombatFormation(Map map)
+        {            
+            for (int i = 0; i < myNavy.Length; i++)
             {
-                while (true)
+                for (int j = 0; j < myNavy[i].Length; j++)
                 {
-                    Console.WriteLine($"Enter from \"{Map.xCoord[0]}\" to \"{Map.xCoord[Map.width - 1]}\" character for X coordinate");
-                    var x = Console.ReadLine();
-                    if (string.IsNullOrEmpty(x) || string.IsNullOrWhiteSpace(x) || int.TryParse(x, out int d) || x.Length > 1 || !Map.xCoord.Contains(x))
+                    while (true)
                     {
-                        Console.WriteLine("You enter wrong character for X coordinate");
-                        continue;
+                        Console.WriteLine($"Enter from \"{Map.X_Coord[0]}\" to \"{Map.X_Coord[Map.SIZE - 1]}\" character for X coordinate");
+                        var x = Console.ReadLine();
+                        if (string.IsNullOrEmpty(x) || string.IsNullOrWhiteSpace(x) || int.TryParse(x, out int d) || x.Length > 1 || !Map.X_Coord.Substring(0, 10).Contains(x))
+                        {
+                            Console.WriteLine("You enter wrong character for X coordinate");
+                            continue;
+                        }
+                        myNavy[i][j].X = x.ToCharArray()[0];
+                        break;
                     }
-                    oneDeck[i].X = x.ToCharArray()[0];
-                    break;                   
-                }
-                while (true)
-                {
-                    Console.WriteLine($"Enter from \"1\" to \"{Map.height}\" integer for Y coordinate");
-                    var y = Console.ReadLine();
-                    if (!int.TryParse(y, out int d) || d > 10 || d < 1)
+                    while (true)
                     {
-                        Console.WriteLine("You enter wrong character for Y coordinate");
-                        continue;
+                        Console.WriteLine($"Enter from \"1\" to \"{Map.SIZE}\" integer for Y coordinate");
+                        var y = Console.ReadLine();
+                        if (!int.TryParse(y, out int d) || d > 10 || d < 1)
+                        {
+                            Console.WriteLine("You enter wrong character for Y coordinate");
+                            continue;
+                        }
+                        myNavy[i][j].Y = d;
+                        break;
                     }
-                    oneDeck[i].Y = d;
-                    break;                    
-                }
-                while (true)
-                {
-                    Console.WriteLine($"Enter one of the following characters \"{(char)Direction.NORTH}\", \"{(char)Direction.SOUTH}\", \"{(char)Direction.WEST}\" or \"{(char)Direction.EAST}\" for initialization of ship direction field");
-                    var d = Console.ReadLine();
-                    if (string.IsNullOrEmpty(d) || string.IsNullOrWhiteSpace(d) || int.TryParse(d, out int dir) || d.Length > 1 || Enum.IsDefined(typeof(Direction), d))
+                    while (true)
                     {
-                        Console.WriteLine("You enter wrong character for ship direction");
-                        continue;
+                        Console.WriteLine($"Enter one of the following characters \"{(char)Direction.NORTH}\", \"{(char)Direction.SOUTH}\", \"{(char)Direction.WEST}\" or \"{(char)Direction.EAST}\" for initialization of ship direction field");
+                        var d = Console.ReadLine();
+                        if (string.IsNullOrEmpty(d) || string.IsNullOrWhiteSpace(d) || int.TryParse(d, out int dir) || d.Length > 1 || !Enum.IsDefined(typeof(Direction), (int)d.ToCharArray()[0]))
+                        {                           
+                            Console.WriteLine("You enter wrong character for ship direction");
+                            continue;
+                        }
+                        myNavy[i][j].Dir = d.ToCharArray()[0];
+                        break;
                     }
-                    oneDeck[i].Dir = d.ToCharArray()[0];
-                    break;
-                }
-            }            
+                    KeyValuePair<char, int> pair = KeyValuePair.Create(myNavy[i][j].X, myNavy[i][j].Y);
+                    CoordinatesList.Add(pair);
+                    Console.Clear();
+                    map.Render();
+                }               
+            }
         }
     }
 }
